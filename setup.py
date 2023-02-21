c5d0af638383c09a3c96f39e3ba5f36f3845183c
Karthik Raj <Karthik.Raj@course5i.com>
2023-02-21 17:41:40 +0530

    penguin-cookiecutter and yaml

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='the project is to demonstrate mlops using mlflow',
+    author='Course5i',
+    license='MIT',
+)
